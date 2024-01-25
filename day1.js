bf1854292a653123d2ec4fec45bb50cb559dd687
kamruzzaman1234 <dmkkaarr.labib980@gmail.com>
2024-01-26 00:32:38 +0600

    First Js Code

---
+++ day1.js
@@ -0,0 +1,30 @@
+// First Task
+var num1 = 1000
+var num2 = 700
+
+var totalResult = num1 - num2
+console.log("First Task result " + "=" + totalResult);
+
+//Second Task
+var mark1 = 75.25;
+var mark2 = 65;
+var mark3 = 80;
+var mark4 = 35.45;
+var mark5 = 99.50
+var markResult = mark1 + mark2 + mark3 + mark4 + mark5
+var resultMark = markResult / 5
+console.log("Second Task" +  resultMark.toFixed(2))
+
+
+// Third Task
+
+var num3 = 24
+var num4 = 5
+var result = num3 % num4
+console.log("Third Task Result"+ "=" + result)
+// Four Task
+var a = isNaN(10)
+var b = isNaN(2-10)
+
+console.log(a)
+console.log(b)
